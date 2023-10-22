3259f91254bd0ce1feefb98b3089b3359f25ab56
algrit <algrigorenko91@gmail.com>
2023-10-22 23:16:38 +0200

    registration and login tests failed :(

--- education/views.py
+++ education/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from users.forms import LoginForm
 
 def index(request):
-    return render(request, 'education/index.html')
+    message = ''
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                message = f'Hello {user.username}! You have been logged in'
+            else:
+                message = 'Login failed!'
+    return render(request, 'education/index.html', context={'form': form, 'message': message})
