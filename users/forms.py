0142bd0477b5db010f950feee820fe6cf7c65497
algrit <algrigorenko91@gmail.com>
2023-11-01 13:40:06 +0100

    passwords fields inherit successfully

--- users/forms.py
+++ users/forms.py
@@ -1,34 +1,20 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth import password_validation
+
 
 class UserRegisterForm(UserCreationForm):
-    username = UsernameField(widget=forms.TextInput(attrs={'placeholder': 'username'}))
-    password1 = forms.CharField(
-        label=_("Password"),
-        strip=False,
-        widget=forms.PasswordInput(attrs={"autocomplete": "new-password",
-                                          'placeholder': 'password'}),
-        help_text=password_validation.password_validators_help_text_html(),
-    )
-    password2 = forms.CharField(
-        label=_("Password confirmation"),
-        widget=forms.PasswordInput(attrs={"autocomplete": "new-password",
-                                          'placeholder': 'repeat the password'}),
-        strip=False,
-        help_text=_("Enter the same password as before, for verification."),
-    )
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'username'
+        self.fields['password1'].widget.attrs['placeholder'] = 'password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'password confirmation'
 
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
-        # widgets = {
-        #     'username': forms.TextInput(attrs={'placeholder': 'username'}),
-        #     'password1': forms.TextInput(attrs={'placeholder': 'TEST'}),
-        #     'password2': forms.PasswordInput(attrs={'placeholder': 'TEST'}),
-        # }
+        fields = ['username']
+
+
 
 
 class LoginForm(forms.Form):
