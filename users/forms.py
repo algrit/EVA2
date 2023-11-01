24837c656c6a3a2b0873079d47faa8e91d9dcf8e
algrit <algrigorenko91@gmail.com>
2023-11-01 22:12:16 +0100

    tooltip added

--- users/forms.py
+++ users/forms.py
@@ -6,15 +6,15 @@ from django.contrib.auth.models import User
 class UserRegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['placeholder'] = 'username'
+        self.fields['username'].widget.attrs.update(
+            {'placeholder': 'username', 'data-bs-toggle': 'tooltip',
+             'title': 'Test tooltip. Will be changed later. Now default Django constraints'})
         self.fields['password1'].widget.attrs['placeholder'] = 'password'
         self.fields['password2'].widget.attrs['placeholder'] = 'password confirmation'
 
     class Meta:
         model = User
         fields = ['username']
-
-
 
 
 class LoginForm(forms.Form):
