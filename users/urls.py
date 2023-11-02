3ec86b9c483389dca2c69ed16d3efa78df377339
algrit <algrigorenko91@gmail.com>
2023-11-02 12:55:14 +0100

    transitional page after success registration added

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from .views import register
+from .views import register, register_success
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('success/', register_success, name='register'),
     path('login/', LoginView.as_view(template_name='registration/login.html', extra_context={'next': '/edu/my/'})),
     path('logout/', LogoutView.as_view(next_page='/users/login/'), name='logout'),
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -5,15 +6,22 @@ from .forms import UserRegisterForm
 
 
 def register(request):
+    """It's a common view function, but I decided to add low-level authenticate just after registration
+    High-level authenticate based on built-in classes is in education/view.py"""
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/edu/my/')
+            username = request.POST['username']
+            password = request.POST['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return HttpResponseRedirect('/users/success/')
     else:
         form = UserRegisterForm()
     return render(request, 'registration/register.html', context={'form': form})
 
 
-def add_info(request):
-    return render(request, 'registration/add_info.html')
+def register_success(request):
+    username = request.user
+    return render(request, 'registration/success.html', context={'username': username})
