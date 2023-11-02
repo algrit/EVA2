2217291739f21c1fe4ad86a958f4f263a1503889
algrit <algrigorenko91@gmail.com>
2023-11-02 13:04:35 +0100

    navbar logout added

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('success/', register_success, name='register'),
     path('login/', LoginView.as_view(template_name='registration/login.html', extra_context={'next': '/edu/my/'})),
-    path('logout/', LogoutView.as_view(next_page='/users/login/'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='edu/my/'), name='logout'),
 
 ]
