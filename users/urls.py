e627e72175e970e21aa3827f0232767d434eb617
algrit <algrigorenko91@gmail.com>
2023-12-20 23:34:34 +0300

    forgot password line added to the login window

--- users/urls.py
+++ users/urls.py
@@ -8,25 +8,29 @@ from .views import register, acc_settings, success_signin, success_password_chan
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('success/', success_signin),
     path('login/', LoginView.as_view(template_name='users/login.html', authentication_form=ModalLoginForm)),
     path('logout/', LogoutView.as_view(next_page='/edu/my/')),
-    path('success/password_changed/', success_password_changed),
-    path('success/', success_signin),
     path('account/', acc_settings, name='acc_settings'),
-    path('password/reset/<str:uidb64>/<str:token>/',
-         PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),
-         name='password_reset_confirm'),  # name is using in password_reset_email.html
-    path('password/reset/done/', PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
-         name='password_reset_complete'),
     path('password_change/',
          PasswordChangeView.as_view(template_name='users/password_change.html', form_class=PwdChangeForm,
                                     success_url='/users/success/password_changed/'),
          name='password_change'),  # name is using in acc_settings.html
+    path('success/password_changed/', success_password_changed),
+
     path('password_reset/done/', PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
          name='password_reset_done'),  # name is using in PasswordResetView
     path('password_reset/', PasswordResetView.as_view(template_name='users/password_reset.html',
                                                       subject_template_name='users/password_reset_subject.txt',
                                                       email_template_name='users/password_reset_email.html',
-                                                      from_email='EVAadmin')),
+                                                      from_email='EVAadmin'), name='password_reset'),  # name is using in login.html
+
+
+    path('password/reset/<str:uidb64>/<str:token>/',
+         PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),
+         name='password_reset_confirm'),  # name is using in password_reset_email.html
+    path('password/reset/done/', PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
+         name='password_reset_complete'),
+
 
 ]
