2ed1317d566e111fa4200c64b40fa2da399b57ce
algrit <algrigorenko91@gmail.com>
2023-12-21 21:48:23 +0300

    custom message on the pwd_reset_done page added

--- users/urls.py
+++ users/urls.py
@@ -18,20 +18,21 @@ urlpatterns = [
          name='password_change'),  # name is using in acc_settings.html
     path('success/password_changed/', success_password_changed),
 
-    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+    path('password_reset/done/',
+         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html', extra_context={
+             'title': 'Link to reset your password was sent. Check your E-mail.'}),
          name='password_reset_done'),  # name is using in PasswordResetView
     path('password_reset/', PasswordResetView.as_view(template_name='users/password_reset.html',
                                                       form_class=PwdResetForm,
                                                       subject_template_name='users/password_reset_subject.txt',
                                                       email_template_name='users/password_reset_email.html',
-                                                      from_email='EVAadmin'), name='password_reset'),  # name is using in login.html
-
+                                                      from_email='EVAadmin'), name='password_reset'),
+    # name is using in login.html
 
     path('password/reset/<str:uidb64>/<str:token>/',
          PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),
          name='password_reset_confirm'),  # name is using in password_reset_email.html
     path('password/reset/done/', PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
          name='password_reset_complete'),
-
 
 ]
