c86da24d4615fe0a8183e60b756b27992ff03bab
algrit <algrigorenko91@gmail.com>
2024-01-11 18:44:22 +0300

    media files show correctly

--- education/urls.py
+++ education/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('my/', views.index, name='main'),
     path('courses/', views.subscribe, name='all_courses'),  # name is used in the navbar
