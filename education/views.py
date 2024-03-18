ed5fac94b172c86409ee7036296c0da196c6f43e
algrit <algrigorenko91@gmail.com>
2024-03-18 16:42:38 +0300

    color display works

--- education/views.py
+++ education/views.py
@@ -207,11 +207,20 @@ def test_att_result(request, pk_test_attempt: int):
     q_answers_dict = {}
     questions = Question.objects.filter(test__id=test_att.test.id)  # raw questions
 
-    q_answers_list = []
+    q_list = []
     for question in questions:
-        q_answers_list.append(QuestionAttempt.objects.get(test_attempt=test_att, question=question).answer)
-    questions = [questions, q_answers_list]
-    print(questions)
+        q_list.append({'text': question.question_text, 'green': question.correct_answer})
+        if question.incorrect_answer1 == QuestionAttempt.objects.get(test_attempt=test_att, question=question).answer:
+            q_list[-1]['red'] = question.incorrect_answer1
+            q_list[-1]['white'] = question.incorrect_answer2
+        elif question.incorrect_answer2 == QuestionAttempt.objects.get(test_attempt=test_att, question=question).answer:
+            q_list[-1]['red'] = question.incorrect_answer2
+            q_list[-1]['white'] = question.incorrect_answer1
+        else:
+            q_list[-1]['white1'] = question.incorrect_answer1
+            q_list[-1]['white2'] = question.incorrect_answer2
+
+    print(q_list)
     return render(request, 'education/test_att_result.html', context={'test_att': test_att,
                                                                       'time_spent': time_spent, 'corrects': corrects,
-                                                                      'percent': percent, 'questions': questions})
+                                                                      'percent': percent, 'q_list': q_list})
